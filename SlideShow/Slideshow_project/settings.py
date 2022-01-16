78d87c8429a6206654fb101af02425fd21a61bd2
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2022-01-16 20:39:38 +0330

    fix: login with google added middleware and context_manage

--- SlideShow/Slideshow_project/settings.py
+++ SlideShow/Slideshow_project/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # social auth
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'Slideshow_project.urls'
@@ -73,6 +75,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # social auth
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
